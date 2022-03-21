03c42ba1e0c2d47660083bfbd04676f9b7e66b8c
MattSeligman <matt@modelitup.com>
2022-03-20 23:24:12 -0700

    Revised paragraph, though will need to determine cause of 'undefined' in console log.

--- survey.js
+++ survey.js
@@ -17,25 +17,43 @@ const questions = [
   "Which meal is your favourite (eg: dinner, brunch, etc.) ",
   "What's your favourite thing to eat for that meal? ",
   "Which sport is your absolute favourite? ",
-  "What is your superpower? \nIn a few words, tell us what you are amazing at! "
+  "What is your superpower?"
 ];
 let answers = [];
 
-// the custom response
-const theResponse = ()=> console.log(`\n\nVery cool!\n\nIt's great to meet you ${answers[0]}!\nI'm pleased to hear you enjoy ${answers[1]} and it's cool to know you listen to ${answers[2]} while enjoying it. I'll keep that in mind that your favorite meal is ${answers[4]} which you likely enjoy at ${answers[3]}. The sport of ${answers[5]} eh? (can you tell I'm Canadian). Interesting I'll keep that in mind. Your superpower is ${answers[6]}!? Woah.. I'll have to keep you more near me.`);
+/**
+Contains the custom generated Paragraph from the survey
+** Name = `answers[0]` ? answers[0] : "Answer instead of Undefined"
+** Favorite Activity = `answers[1]`
+** Favorite Listening Material = `answers[2]`
+** Favorite Meal Type/Time = `answers[3]`
+** Favorite Meal = `answers[4]`
+** Favorite Sport = `answers[5]`
+** Superpower = `answers[6]`
+*/
+const theResponse = ()=> {
+  console.log(
+    `\n\nHi! My name is ${answers[0] ? answers[0] : "..ermm private! Call me No Name"} and I am an avid ${answers[1] ? answers[1] : "skipper of typing tasks"}.` +
+  `\nI enjoy ${answers[2] ? answers[2] : "not providing answers"} while doing so. My favorite meal type is ${answers[3] ? answers[3] : "still to be determined"}.` +
+  `\nMy favorite meal is ${answers[4] ? answers[4] : "*stares blankly at screen*"}. My favorite sport is ${answers[5] ? answers[5] : "unknown to many"}. ` +
+  `\nFinally my super secret superpower is ${answers[6] ? answers[6] : "mashing the enter key."}`
+  );
+};
 
 // runs the first question until the end of all the questions
 const askQuestion = (index)=>{
 
   // begins the async question process
   rl.question(questions[index], (answer) => {
+
     answers.push(answer);
             
     // +1 so when it runs itself again it itterates against iself.
     askQuestion(index + 1, ()=> answers.push(answer));
 
+    console.log("answerLength", answers.length, "===", questions.length,"QuestionsLength");
     // if all questions are answered
-    if (answers.length === questions.length - 1) {
+    if (answers.length === questions.length) {
 
       // closes the inputs after all the inputs are taken.
       rl.close();
